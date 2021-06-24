33063f4254dcb71b17f565047768f1540b0f4663
Daniel Sazonov <p.294803@gmail.com>
2021-06-24 19:00:27 +0300

    Implement the deletion of the necessary data

--- main.py
+++ main.py
@@ -9,25 +9,31 @@ c_reset = colorama.Style.RESET_ALL
 def set_metadata(file):
     text = config.LOCALE
     track = EasyID3(file)
-    metadata = dict()
 
+    # getting data from user and editing the metadata of the current file
     for data in text:
         if data in track.keys():
-            print(colorama.Style.BRIGHT + text[data], end=' ')
-            print(colorama.Style.DIM + '({1}): '.format(text[data], track[data][0]), end=' ')
+            print(colorama.Style.BRIGHT + text[data] + c_reset, end=' ')
+            print(colorama.Style.DIM + '({0}): '.format(track[data][0]), end=' ')
             user_input = input()
-            metadata[data] = user_input if user_input else track[data][0]
+            track[data] = user_input if user_input else track[data][0]
 
-    return metadata
+    # todo если надо что-то сохранить, здесь проверка на флаг
+
+    # deleting unnecessary data. wrong approach, will be fixed
+    for data in track:
+        if data not in text:
+            del track[data]
+
+    track.save()
+    return track
 
 
-#
-#
-# def main():
-#     print(set_metadata('./drafts/example2.mp3'))
-#
-#
-# if __name__ == "__main__":
-#     main()
+def main():
+    print(set_metadata('./drafts/example2.mp3'))
 
-print(set_metadata('P:\\id3-editor\\drafts\\example.mp3'))
+
+if __name__ == "__main__":
+    main()
+
+# print(set_metadata('P:\\id3-editor\\drafts\\example.mp3'))
