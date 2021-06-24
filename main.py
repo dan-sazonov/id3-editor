0ec48e5919fd15e12506c0ca899b7d6a14ee21ce
Daniel Sazonov <p.294803@gmail.com>
2021-06-24 21:44:59 +0300

    Implement the ability to ignore parameters

--- main.py
+++ main.py
@@ -8,24 +8,30 @@ colorama.init()
 c_reset = colorama.Style.RESET_ALL
 
 default_data = dict()
+ignored_minimal = {'tracknumber', 'genre'}
 
 
 def set_parser():
     parser = argparse.ArgumentParser(
         prog='id3-editor',
-        description='''The simplest console tool for batch editing of mp3 metadata.''',
+        description='''The simplest console tool for batch editing of mp3 metadata''',
         epilog='''(c) 2021, Dan Sazonov. Apache-2.0 License'''
     )
+
+    parser.add_argument('-m', '--minimal', action='store_true', default=False,
+                        help='set only title, artist, album and genre')
     return parser
 
 
-def set_metadata(file):
+def set_metadata(file, *ignore):
     text = config.LOCALE
     track = EasyID3(file)
+    actual_data = set(track.keys())
+    ignored_data = set(ignore)
 
     # getting data from user and editing the metadata of the current file
     for data in text:
-        if data in track.keys():
+        if data in actual_data and data not in ignored_data:
             print(colorama.Style.BRIGHT + text[data] + c_reset, end=' ')
             print(colorama.Style.DIM + '({0}): '.format(track[data][0]), end=' ')
             user_input = input()
@@ -34,8 +40,8 @@ def set_metadata(file):
     # todo если надо что-то сохранить, здесь проверка на флаг
 
     # deleting unnecessary data. wrong approach, will be fixed
-    for data in track:
-        if data not in text:
+    for data in actual_data:
+        if data not in text or data in ignored_data:
             del track[data]
 
     track.save()
@@ -45,9 +51,7 @@ def set_metadata(file):
 def main():
     cli_parser = set_parser()
     namespace = cli_parser.parse_args(sys.argv[1:])
-
-    print(namespace)
-    print(set_metadata('./drafts/example2.mp3'))
+    print(set_metadata('./drafts/example2.mp3', 'title'))
 
 
 if __name__ == "__main__":
