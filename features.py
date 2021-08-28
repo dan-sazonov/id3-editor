6502bb2dd18dcdf57df2100b04849509f6fff37e
Daniel Sazonov <p.294803@gmail.com>
2021-08-28 20:31:54 +0300

    Create validate_year

--- features.py
+++ features.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+
 def remove_brackets(s: str):
     """
     Remove text in the square brackets
@@ -7,3 +10,17 @@ def remove_brackets(s: str):
     """
     (split_symbol, index) = (']', 1) if s.startswith('[') else ('[', 0) if s.endswith(']') else ('\n', 0)
     return s.split(split_symbol)[index].strip(' ')
+
+
+def validate_year(year):
+    """
+    Validate the value of year
+
+    :param year: the value that needs to be validated
+    :return: the value of the year param, if this value is a digit and is greater than 1800 and not greater than the
+    current year, the empty string otherwise
+    """
+    if year.isdigit() and 1800 <= int(year) <= date.today().year:
+        return year
+    else:
+        return ''
