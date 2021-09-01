9bd63c5365c47a0a5754090295516ea64646df47
Daniel Sazonov <p.294803@gmail.com>
2021-09-01 20:58:42 +0300

    Simplify code

--- main.py
+++ main.py
@@ -169,11 +169,10 @@ def edit_files(files: dict, path: str, clear_all: bool, do_rename: bool):
         # save metadata and rename file
         track.save()
         if do_rename:
-            # fucking terrible code, i know
-            artist_tmp = track["artist"][0].replace(" ", "_").replace("-", "_")
-            title_tmp = track["title"][0].replace(" ", "_").replace("-", "_")
-            file_name_tmp = f'{artist_tmp}-{title_tmp}.mp3'.replace("/", "")
-            os.rename(current_path, f'{"/".join(current_path.split("/")[:-1])}/{file_name_tmp}')
+            tmp_data = [track[i][0].translate(str.maketrans({' ': '_', '-': '_', '/': ''}))
+                        for i in ('artist', 'title')]
+            file_name_tmp = '{0}-{1}.mp3'.format(*tmp_data)
+            os.rename(current_path, f'{path}/{file_name_tmp}')
             renamed[file] = file_name_tmp
 
     return renamed
