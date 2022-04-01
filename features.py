5782469827397337c226884cc5ca444a92ea0cbc
Dan Sazonov <p.294803@gmail.com>
2022-04-01 23:37:26 +0300

    Add docstrings

--- features.py
+++ features.py
@@ -1,13 +1,14 @@
 import pyperclip
+from mutagen.easyid3 import EasyID3
 
 
 def get_new_filename(artist: str, title: str) -> str:
     """
     Get new filename for the track. Remove all symbols, replace spaces with underscores
 
-    :param artist:
-    :param title:
-    :return:
+    :param artist: artist of this track
+    :param title: title of this track
+    :return: new filename looks like 'artist-file_name'
     """
 
     tmp_data = []
@@ -17,7 +18,13 @@ def get_new_filename(artist: str, title: str) -> str:
     return '{0}-{1}.mp3'.format(*tmp_data)
 
 
-def get_track_title(track):
+def get_track_title(track: EasyID3) -> list[str, str]:
+    """
+    Get artist and title from the track
+
+    :param track: mutagen object, metadata of this track
+    :return: list[artist, title]
+    """
     clip = []
     for data in 'artist', 'title':
         if data in track.keys():
@@ -25,7 +32,13 @@ def get_track_title(track):
     return clip
 
 
-def copy_track_title(track):
+def copy_track_title(track: EasyID3) -> None:
+    """
+    Save artist and title of this track to the clipboard
+
+    :param track: mutagen object, metadata of this track
+    :return: None
+    """
     artist, title = get_track_title(track)
     clip = f'{artist} - {title}'
     pyperclip.copy(clip)
