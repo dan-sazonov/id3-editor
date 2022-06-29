816d29eed554e213f7c84af533b2394b655c1ac8
Dan Sazonov <p.294803@gmail.com>
2022-06-30 02:13:09 +0300

    Implement translator

--- scrap.py
+++ scrap.py
@@ -1,10 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 from validator import validate_for_url
+from deep_translator import GoogleTranslator
+
+
+def _translate(s):
+    return GoogleTranslator(source='auto', target='en').translate(s)
 
 
 def get_album_title(band, artist):
-    band, artist = validate_for_url((band, artist))
+    band, artist = validate_for_url((_translate(band), _translate(artist)))
     url = f'https://genius.com/{band}-{artist}-lyrics'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
@@ -12,11 +17,9 @@ def get_album_title(band, artist):
     ans = soup.select('[href="#primary-album"]')
     return '' if not ans else ans[0].text.split(' (')[0]
 
-# todo переводить с русского название трека и группу
-
 
 if __name__ == '__main__':
-    tracks = [('Blink-182', 'What’s My Age Again?'), ('saypink!', 'AND I WANT')]
+    tracks = [('Blink-182', 'What’s My Age Again?'), ('saypink!', 'А Я ХОЧУ')]
 
     for i in tracks:
         print(get_album_title(*i))
