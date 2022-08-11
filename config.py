5867106cc4f3dbbce64b0a72664ecff08cc5f55c
Dan Sazonov <p.294803@gmail.com>
2022-08-12 00:49:42 +0300

    Create env var DEV_MODE

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 import colorama
 
@@ -14,7 +15,7 @@ AUTHOR = 'Dan Sazonov'
 ENABLE_PARSER = True
 
 # if True, dev mode will be enabled
-DEV_MODE = False
+DEV_MODE = os.getenv('DEV_MODE') == '1' or os.getenv('DEV_MODE').lower() == 'true'
 
 # 'metadata key': 'text for CLI'
 # don't change the keys! values could be translated to other languages
